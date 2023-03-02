c99e4b93344fa44eebf4bcf132457f967d04dab9
Rawrs64 <91361104+Rawrs64@users.noreply.github.com>
2023-03-02 04:59:15 -0500

    Update Main.java

--- Altered/Assignment1Attempt2/Main.java
+++ Altered/Assignment1Attempt2/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws JsonSyntaxException, IOException {
         
         // Start GUI
-        new GUI();
+        //new GUI();
 
         // Make a new hashtable and populate it with businesses
         CustomHashTable businesses = new CustomHashTable();
@@ -20,6 +20,22 @@ public class Main {
 
         businesses.scoreBusinesses();
         System.out.println("Done with the Scoring");
+
+        Business outputOne = new Business();
+        Business outputTwo = new Business();
+        Business outputThree = new Business();
+        double latitude = 38.575764;
+        double longitude = -121.478851;
+
+        System.out.println("The default latitude and longitude is Lat: " + latitude + " Long: " + longitude);
+
+
+        outputOne = businesses.locateClosestBusinesses(latitude, longitude);
+        outputTwo = businesses.locateSecondClosestBusiness(outputOne.getName(), latitude, longitude);
+        outputThree = businesses.locateThirdClosestBusinesses(outputOne.getName(), outputTwo.getName(), latitude, longitude);
+        System.out.println("The First closest business is: " + outputOne.getName());
+        System.out.println("The Second closest business is: " + outputTwo.getName());
+        System.out.println("The Third closest business is: " + outputThree.getName());
 
         System.out.println("Done");
 
